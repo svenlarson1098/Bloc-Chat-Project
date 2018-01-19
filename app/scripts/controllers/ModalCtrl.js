d484ea4eb044aff77579254d447991e0abd51610
svenlarson1098 <svenlarson1098@gmail.com>
2018-01-19 12:25:41 -0800

    Lesson 1 Progress

---
+++ app/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,19 @@
+(function() {
+    function  ModalCtrl($uibModalInstance, Room) {
+        this.newRoom = "";
+
+        this.cancel = function() {
+          $uibModalInstance.dismiss('cancel');
+        };
+
+        this.create = function() {
+          Room.addRoom(this.newRoom);
+          $uibModalInstance.close();
+        };
+    };
+
+    angular
+      .module('blocChat')
+      .controller('ModalCtrl', ['uibModalInstance', 'Room', ModalCtrl]);
+
+})();
